39447b14c5dccd8ae2244d80e6863b9e77ef76a0
Hizbullah Abdul Aziz Jabbar <archbung@gmail.com>
2020-10-12 07:22:38 +0200

    Use nix-doom-emacs

---
+++ dev/emacs/default.nix
@@ -0,0 +1,28 @@
+{ config, pkgs, ... }:
+
+let doom-emacs = pkgs.callPackage (builtins.fetchTarball "https://github.com/vlaci/nix-doom-emacs/archive/master.tar.gz")
+    {
+      doomPrivateDir = ./doom;
+      emacsPackagesOverlay = self: super: {
+        magit-delta = super.magit-delta.overrideAttrs (super: {
+          buildInputs = super.buildInputs ++ [ pkgs.git ];
+        });
+      };
+    };
+in
+{
+  home = {
+    packages = with pkgs; [
+      ripgrep fd clang
+    ] ++ [ doom-emacs ];
+
+    file.".emacs.d/init.el".text =
+      ''
+      (load "default.el")
+      '';
+  };
+
+  programs.emacs.enable = true;
+
+  services.emacs.socketActivation.enable = true;
+}
