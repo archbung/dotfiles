f30dd0122c739455802b1ea6b03f3913ccaafe6a
Hizbullah Abdul Aziz Jabbar <archbung@gmail.com>
2021-01-10 20:36:55 +0100

    Set gtk fonts

--- hm/fonts.nix
+++ hm/fonts.nix
@@ -4,5 +4,12 @@
   home.packages = with pkgs; [
     noto-fonts
     fira-code
+    symbola
   ];
+
+  fonts.fontconfig.enable = true;
+
+  gtk.font = {
+    name = "Noto Serif Display 16";
+  };
 }
