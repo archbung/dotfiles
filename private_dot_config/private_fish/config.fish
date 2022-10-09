afc510bd302db041cc4fc61990891d71cebe95a5
Hizbullah Abdul Aziz <archbung@tutanota.com>
2022-10-09 09:59:39 +0200

    Add fish config

---
+++ private_dot_config/private_fish/config.fish
@@ -0,0 +1,9 @@
+# vim:set ts=2 sts=2 sw=2 et:
+if status is-interactive
+  set -gx GPG_TTY $(tty)
+  set -gx EDITOR "nvim"
+
+  keychain --eval --quiet -Q id_github id_gitlab | source
+  starship init fish | source
+  zoxide init fish | source
+end
