0c52210c197e3a3c09b1003130e325e475fab6e8
Hizbullah Abdul Aziz Jabbar <archbung@gmail.com>
2023-06-19 15:00:48 +0700

    Add picom and dunst

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -4,9 +4,16 @@ if status is-interactive
   set -gx EDITOR "nvim"
   set -gx BROWSER "firefox-developer-edition"
 
+  set -U fish_greeting
   fish_vi_key_bindings
 
-  keychain --eval --quiet -Q id_github | source
+  fish_add_path ~/.local/bin ~/.ghcup/bin ~/.cargo/bin
+
+  keychain --eval --quiet -Q id_github id_gitlab | source
   starship init fish | source
   zoxide init fish | source
+
+  if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
+    exec startx -- -keeptty
+  end
 end
