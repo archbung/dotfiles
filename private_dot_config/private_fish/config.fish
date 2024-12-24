f4614f03dcd8cf2e8e95a4b292de1b3ca1f4dcee
Hizbullah Abdul Aziz Jabbar <archbung@gmail.com>
2024-12-24 17:43:11 +0700

    Update fish config

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,12 +1,12 @@
-# vim:set ts=2 sts=2 sw=2 et:
 if status is-interactive
-  set -gx GHCUP_USE_XDG_DIRS 1
   set -gx GPG_TTY $(tty)
 
-  zoxide   init fish                              | source
-  starship init fish                              | source
-  keychain --eval --quiet -Q id_github id_gitlab  | source
-
-  fish_add_path -p ~/.local/bin
+  fish_add_path -p ~/.ghcup/bin ~/.local/bin ~/.elan/bin
   fish_vi_key_bindings
+
+  starship init fish                              | source
+  zoxide init fish                                | source
+  keychain --eval --quiet -Q id_github id_gitlab  | source
+  rbenv init - fish                               | source
 end
+
