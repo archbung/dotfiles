ec736da215b9edd33c8991fa997d23da0e66fa0b
Hizbullah Abdul Aziz Jabbar <archbung@icloud.com>
2026-05-06 10:27:31 +0700

    Fix ruby-lsp

---
+++ dot_config/nvim/lua/plugins/ruby-lsp.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        ruby_lsp = {
+          mason = false,
+          cmd = { vim.fn.expand("~/.rbenv/shims/ruby-lsp") },
+        },
+        rubocop = {
+          mason = false,
+          cmd = { vim.fn.expand("~/.rbenv/shims/rubocop"), "--lsp" },
+        },
+      },
+    },
+  },
+}
